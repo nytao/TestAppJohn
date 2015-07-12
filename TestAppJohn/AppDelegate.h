add678493115be0303c8584acef38f76edae45d8
William <william@Williams-MacBook-Air.local>
2015-07-12 15:34:23 -0400

    Initial Commit

---
+++ TestAppJohn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestAppJohn
+//
+//  Created by William on 7/12/15.
+//  Copyright (c) 2015 William. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestAppJohn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestAppJohn
+//
+//  Created by William on 7/12/15.
+//  Copyright (c) 2015 William. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
